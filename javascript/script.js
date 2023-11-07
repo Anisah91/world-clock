67d40824c399fb77149e1cb5702a3f373d486091
Anisah91 <137075663+Anisah91@users.noreply.github.com>
2023-11-07 14:49:34 +0100

    added js single location

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,24 @@
+// Doha Time
 
+function updateDohaTime() {
+  let dohaElement = document.querySelector("#doha");
+  let dohaDateElement = document.querySelector("#doha .date");
+  let dohaTime = moment().tz("Asia/Qatar");
+  let dohaTimeElement = document.querySelector("#doha .time");
+  dohaTimeElement.innerHTML = dohaTime.format("h:mm:ss [<small>] A [</small>]");
+  dohaDateElement.innerHTML = dohaTime.format("MMMM Do YYYY");
+}
+updateDohaTime();
+setInterval(updateDohaTime, 1000);
+
+// Gaza Time
+function updateGazaTime() {
+  let gazaElement = document.querySelector("#gaza");
+  let gazaDateElement = document.querySelector("#gaza .date");
+  let gazaTime = moment().tz("Asia/Gaza");
+  let gazaTimeElement = document.querySelector("#gaza .time");
+  gazaTimeElement.innerHTML = gazaTime.format("h:mm:ss[<small>] A [</small>]");
+  gazaDateElement.innerHTML = gazaTime.format("MMMM Do YYYY");
+}
+updateGazaTime();
+setInterval(updateGazaTime, 1000);
